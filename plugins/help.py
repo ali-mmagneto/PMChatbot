87af58a3a8114fe192714d21d39018fe83eafc69
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-01-03 18:12:06 +0300

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -31,3 +31,25 @@ async def help_me(bot, message):
             info.dc_id
         )
     )
+
+@Client.on_message(filters.private & filters.text & filters.media))
+async def help_me(bot, message):
+    if message.from_user.id == Config.ADMIN:
+        return
+    info = await bot.get_users(user_ids=message.from_user.id)
+    await bot.send_message(
+        chat_id=message.chat.id,
+        text=Presets.NOT_TEXT.format(info.first_name)
+    )
+    await bot.send_message(
+        chat_id=Config.ADMIN,
+        text=Presets.USER_DETAILS.format(
+            info.first_name,
+            info.last_name,
+            info.id, info.username,
+            info.is_scam,
+            info.is_restricted,
+            info.status,
+            info.dc_id
+        )
+    )
